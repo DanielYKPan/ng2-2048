fe6025e83026b0194bf8b643d2867d4469f59e44
Yaokun Pan <myron.yk.pan@gmail.com>
2017-01-23 13:22:24 +1100

    Added tile class

---
+++ src/app/+game/tile/tile.ts
@@ -0,0 +1,44 @@
+/**
+ * tile
+ */
+
+import { uuid } from "../uuid";
+
+
+export interface Coordination {
+    x: number;
+    y: number;
+}
+
+export class Tile {
+
+    /* Property id */
+    private id: string;
+
+    get Id(): string {
+        return this.id;
+    }
+
+    public coordination: Coordination;
+    public value: number;
+    public originalCoordi: Coordination;
+    public merged: boolean;
+    public shouldDump: boolean;
+
+
+    constructor( coordination: Coordination, value?: number ) {
+        this.id = uuid();
+        this.coordination = coordination;
+        this.value = value || 2;
+        this.merged = false;
+        this.shouldDump = false;
+    }
+
+    backupPosition() {
+        this.originalCoordi = this.coordination;
+    }
+
+    resetMergedStatus() {
+        this.merged = false;
+    }
+}
