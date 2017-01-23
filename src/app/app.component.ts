b3899845609058f12449099e6deaa0b8d1648e7c
Yaokun Pan <myron.yk.pan@gmail.com>
2017-01-23 13:08:13 +1100

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+/*
+ * Angular 2 decorators and services
+ */
+import {
+    Component,
+    OnInit,
+    ViewEncapsulation
+} from '@angular/core';
+import { AppState } from './app.service';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+    selector: 'app',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: [
+        './app.component.scss'
+    ],
+    template: `
+    <nav>
+      <a [routerLink]=" ['./'] " routerLinkActive="active">
+        Index
+      </a>
+      <a [routerLink]=" ['./game'] " routerLinkActive="active">
+        Game
+      </a>
+    </nav>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+
+    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+
+    <footer>
+      <span>WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a></span>
+      <div>
+        <a [href]="url">
+          <img [src]="angularclassLogo" width="25%">
+        </a>
+      </div>
+    </footer>
+  `
+})
+export class AppComponent implements OnInit {
+    public angularclassLogo = 'assets/img/angularclass-avatar.png';
+    public name = 'Angular 2 Webpack Starter';
+    public url = 'https://twitter.com/AngularClass';
+
+    constructor( public appState: AppState ) {
+    }
+
+    public ngOnInit() {
+        console.log('Initial App State', this.appState.state);
+    }
+
+}
