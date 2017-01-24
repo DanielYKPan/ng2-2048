d341be41cbf233668e85513c2fb541deccefbd0a
Yaokun Pan <myron.yk.pan@gmail.com>
2017-01-24 12:00:44 +1100

    Added backgound-image in game.component

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,10 @@ module.exports = {
         require('autoprefixer')({
             browsers: ['last 8 version', '> 1%', 'ie 9', 'ie 8', 'ie 7', 'ios 6', 'Firefox <= 20'],
             cascade: false
+        }),
+        require('postcss-assets')({
+            loadPaths: ['assets'],
+            basePath: 'src/'
         })
     ]
 };
